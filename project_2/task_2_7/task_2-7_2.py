6b01dd5c17caf0b1e3304008308518387aa96e49
lizapaschenko <lizapaschenko2007@gmail.com>
2026-02-24 13:03:45 +0300

    Add files via upload

---
+++ project_2/task_2_7/task_2-7_2.py
@@ -0,0 +1,10 @@
+sequences = ["ATATACGCCGTAA", "CTTCGGNGGCA"]
+
+for seq in sequences:
+    print(f"Последовательность: {seq}")  
+    print("Построчно:")
+    for letter in seq:                    
+        print(letter)
+    
+
+print("Цикл выполнен")
